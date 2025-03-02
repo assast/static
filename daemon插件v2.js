d347ac8ca8272d05e9ad0cc3ab7b71ddd95f638f
guotao <guotao09@126.com>
2025-03-02 18:59:05 +0800

    1

--- daemon插件v2.js
+++ daemon插件v2.js
@@ -1167,9 +1167,9 @@ function addMsg(msg, type) {
     msgBox.value = msg.replace(/\\n/g, '\n');
 
     // 动态调整 textarea 的高度
-    msgBox.style.height = 'auto'; // 先设置为 auto，以便根据内容计算高度
+    // msgBox.style.height = 'auto'; // 先设置为 auto，以便根据内容计算高度
     // msgBox.style.height = Math.min(msgBox.scrollHeight, 100) + 'px'; // 限制最大高度为 200px
-    // msgBox.style.height = '100px'; // 先设置为 auto，以便根据内容计算高度
+    msgBox.style.height = '100px'; // 先设置为 auto，以便根据内容计算高度
 
     if (type && type == 'error') {
         msgBox.className = 'daemon-msg daemon-msg-fail';
