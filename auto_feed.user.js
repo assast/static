ff9f9997075cbbdae8624bb03a4399287b139ffa
guotao <guotao09@126.com>
2025-04-20 16:23:15 +0800

    1

--- auto_feed.user.js
+++ auto_feed.user.js
@@ -94,7 +94,7 @@
 // @require      https://greasyfork.org/scripts/444988-music-helper/code/music-helper.js?version=1268106
 // @icon         https://kp.m-team.cc//favicon.ico
 // @run-at       document-end
-// @version      1.0.0.63
+// @version      1.0.0.64
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setClipboard
 // @grant        GM_setValue
